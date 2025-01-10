27bac5265abd6f6415bfc5791207df41301a9f22
unknown <jgarcia@unbcorp.cl>
2025-01-09 20:56:24 -0500

    <styles>: Styles for the image hero

--- src/components/hero.tsx
+++ src/components/hero.tsx
@@ -28,16 +28,20 @@ export default function Hero() {
               {/* Lado izquierdo */}
               <div className="sm:text-center lg:text-left lg:col-span-6">
                 <h1 className="text-4xl tracking-tight text-primary font-extrabold sm:text-4xl md:text-6xl">
-                  <span className="flex">Mira nuestros nuevos beneficios para los miembros</span>
+                  <span className="flex">
+                    Mira nuestros nuevos beneficios para los miembros
+                  </span>
                 </h1>
                 <p className="mt-4 text-base text-black sm:text-lg md:text-xl">
-                  Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptate delectus veritatis perspiciatis, alias mollitia, quaerat numquam aliquam eos provident harum magnam dicta nostrum ipsam similique quibusdam, at neque laboriosam iusto..
+                  Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+                  Voluptate delectus veritatis perspiciatis, alias mollitia,
+                  quaerat numquam aliquam eos provident harum magnam dicta
+                  nostrum ipsam similique quibusdam, at neque laboriosam iusto..
                 </p>
-
               </div>
 
               {/* Imagen */}
-              <div className="lg:col-span-6 flex justify-center items-end absolute bottom-0 left-1/2 transform -translate-x-1/2 lg:relative lg:left-0 lg:transform-none">
+              <div className="lg:col-span-6 flex justify-center items-end absolute bottom-0 left-1/2 transform -translate-x-1/2 lg:relative lg:left-0 lg:transform-none w-full">
                 <img
                   className="w-full h-auto max-h-[40vh] sm:max-h-[50vh] md:max-h-[60vh] object-contain"
                   src="/images/heros.png"
